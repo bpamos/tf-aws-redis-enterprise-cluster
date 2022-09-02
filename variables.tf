9cbff5d7e54b8019a0e37079b37cf701f3549124
brandonamos <bpamos21@gmail.com>
2022-09-02 07:01:52 -0600

    add outputs and variables

--- variables.tf
+++ variables.tf
@@ -15,6 +15,11 @@ variable "owner" {
     description = "owner tag name"
 }
 
+# VPC
+variable "vpc_cidr" {
+    description = "vpc-cidr"
+}
+
 # DNS
 variable "dns_hosted_zone_id" {
     description = "DNS hosted zone Id"
@@ -113,83 +118,310 @@ variable "re-volume-size" {
   default     = "150"
 }
 
-# cluster commands
+#### Security
 
-variable "re_cluster_username" {
-    description = "redis enterprise cluster username"
-    default     = "demo@redislabs.com"
+variable "open-nets" {
+  type        = list(any)
+  description = "CIDRs that will have access to everything"
+  default     = []
 }
 
-variable "re_cluster_password" {
-    description = "redis enterprise cluster password"
-    default     = "123456"
+variable "allow-public-ssh" {
+  description = "Allow SSH to be open to the public - disabled by default"
+  default     = "0"
 }
 
-# memtier commands
-
-variable "memtier_data_input_1" {
-  description = "memtier data input (1st)"
-  default = "memtier_benchmark -x 3 -n 180000 -c 1 -t 1 --ratio=1:0 --data-size=80 --key-maximum=180000 --pipeline=1000 --key-pattern=S:S --hide-histogram"
+variable "internal-rules" {
+  description = "Security rules to allow for connectivity within the VPC"
+  type        = list(any)
+  default = [
+    {
+      type      = "ingress"
+      from_port = "22"
+      to_port   = "22"
+      protocol  = "tcp"
+      comment   = "SSH from VPC"
+    },
+    {
+      type      = "ingress"
+      from_port = "1968"
+      to_port   = "1968"
+      protocol  = "tcp"
+      comment   = "Proxy traffic (Internal use)"
+    },
+    {
+      type      = "ingress"
+      from_port = "3333"
+      to_port   = "3341"
+      protocol  = "tcp"
+      comment   = "Cluster traffic (Internal use)"
+    },
+    {
+      type      = "ingress"
+      from_port = "3343"
+      to_port   = "3344"
+      protocol  = "tcp"
+      comment   = "Cluster traffic (Internal use)"
+    },
+    {
+      type      = "ingress"
+      from_port = "36379"
+      to_port   = "36380"
+      protocol  = "tcp"
+      comment   = "Cluster traffic (Internal use)"
+    },
+    {
+      type      = "ingress"
+      from_port = "8001"
+      to_port   = "8001"
+      protocol  = "tcp"
+      comment   = "Traffic from application to RS Discovery Service"
+    },
+    {
+      type      = "ingress"
+      from_port = "8002"
+      to_port   = "8002"
+      protocol  = "tcp"
+      comment   = "System health monitoring"
+    },
+    {
+      type      = "ingress"
+      from_port = "8004"
+      to_port   = "8004"
+      protocol  = "tcp"
+      comment   = "System health monitoring"
+    },
+    {
+      type      = "ingress"
+      from_port = "8006"
+      to_port   = "8006"
+      protocol  = "tcp"
+      comment   = "System health monitoring"
+    },
+    {
+      type      = "ingress"
+      from_port = "8443"
+      to_port   = "8443"
+      protocol  = "tcp"
+      comment   = "Secure (HTTPS) access to the management web UI"
+    },
+    {
+      type      = "ingress"
+      from_port = "8444"
+      to_port   = "8444"
+      protocol  = "tcp"
+      comment   = "nginx <-> cnm_http/cm traffic (Internal use)"
+    },
+    {
+      type      = "ingress"
+      from_port = "9080"
+      to_port   = "9080"
+      protocol  = "tcp"
+      comment   = "nginx <-> cnm_http/cm traffic (Internal use)"
+    },
+    {
+      type      = "ingress"
+      from_port = "9081"
+      to_port   = "9081"
+      protocol  = "tcp"
+      comment   = "For CRDB management (Internal use)"
+    },
+    {
+      type      = "ingress"
+      from_port = "8070"
+      to_port   = "8071"
+      protocol  = "tcp"
+      comment   = "Prometheus metrics exporter"
+    },
+    {
+      type      = "ingress"
+      from_port = "9443"
+      to_port   = "9443"
+      protocol  = "tcp"
+      comment   = "REST API traffic, including cluster management and node bootstrap"
+    },
+    {
+      type      = "ingress"
+      from_port = "10000"
+      to_port   = "19999"
+      protocol  = "tcp"
+      comment   = "Database traffic - if manually creating db ports pare down"
+    },
+    {
+      type      = "ingress"
+      from_port = "20000"
+      to_port   = "29999"
+      protocol  = "tcp"
+      comment   = "Database shards traffic - if manually creating db ports pare down"
+    },
+    {
+      type      = "ingress"
+      from_port = "53"
+      to_port   = "53"
+      protocol  = "udp"
+      comment   = "DNS Traffic"
+    },
+    {
+      type      = "ingress"
+      from_port = "5353"
+      to_port   = "5353"
+      protocol  = "udp"
+      comment   = "DNS Traffic"
+    },
+    {
+      type      = "ingress"
+      from_port = "-1"
+      to_port   = "-1"
+      protocol  = "icmp"
+      comment   = "Ping for connectivity checks between nodes"
+    },
+    {
+      type      = "egress"
+      from_port = "-1"
+      to_port   = "-1"
+      protocol  = "icmp"
+      comment   = "Ping for connectivity checks between nodes"
+    },
+    {
+      type      = "egress"
+      from_port = "0"
+      to_port   = "65535"
+      protocol  = "tcp"
+      comment   = "Let TCP out to the VPC"
+    },
+    {
+      type      = "egress"
+      from_port = "0"
+      to_port   = "65535"
+      protocol  = "udp"
+      comment   = "Let UDP out to the VPC"
+    },
+    #    {
+    #      type      = "ingress"
+    #      from_port = "8080"
+    #      to_port   = "8080"
+    #      protocol  = "tcp"
+    #      comment   = "Allow for host check between nodes"
+    #    },
+  ]
 }
 
-variable "memtier_benchmark_1" {
-  description = "memtier benchmark code to run (1st)"
-  default = "memtier_benchmark -x 2 -t 8 -c 100 -n 100 --ratio=1:10000 --data-size=80 --key-maximum=180000 --hide-histogram"
+variable "external-rules" {
+  description = "Security rules to allow for connectivity external to the VPC"
+  type        = list(any)
+  default = [
+    {
+      type      = "ingress"
+      from_port = "53"
+      to_port   = "53"
+      protocol  = "udp"
+      cidr      = ["0.0.0.0/0"]
+    },
+    {
+      type      = "egress"
+      from_port = "0"
+      to_port   = "65535"
+      protocol  = "tcp"
+      cidr      = ["0.0.0.0/0"]
+    },
+    {
+      type      = "egress"
+      from_port = "0"
+      to_port   = "65535"
+      protocol  = "udp"
+      cidr      = ["0.0.0.0/0"]
+    }
+  ]
 }
 
-variable "outfile_name_1" {
-    description = "outfile json name (1st run)"
-    default = "mybenchmarkOutfile.json"
-}
 
-# cluster db commands
 
-variable "redis_db_name_1" {
-    description = "redis enterprise db "
-    default     = "myDB"
-}
 
-variable "redis_db_memory_size_1" {
-    description = "redis enterprise db "
-    default     = 100000000
-}
 
-variable "redis_db_replication_1" {
-    description = "redis enterprise db "
-    default     = "true"
-}
 
-variable "redis_db_sharding_1" {
-    description = "redis enterprise db "
-    default     = "false"
-}
 
-variable "redis_db_shard_count_1" {
-    description = "redis enterprise db "
-    default     = 1
-}
 
-variable "redis_db_proxy_policy_1" {
-    description = "redis enterprise db "
-    default     = "single"
-}
 
-variable "redis_db_shards_placement_1" {
-    description = "redis enterprise db "
-    default     = "dense"
-}
+##############################
 
-variable "redis_db_data_persistence_1" {
-    description = "redis enterprise db "
-    default     = "aof"
-}
 
-variable "redis_db_aof_policy_1" {
-    description = "redis enterprise db "
-    default     = "appendfsync-always"
-}
+# # cluster commands
 
-variable "redis_db_port" {
-    description = "redis enterprise db "
-    default     = 10000
-}
+# variable "re_cluster_username" {
+#     description = "redis enterprise cluster username"
+#     default     = "demo@redislabs.com"
+# }
+
+# variable "re_cluster_password" {
+#     description = "redis enterprise cluster password"
+#     default     = "123456"
+# }
+
+# # memtier commands
+
+# variable "memtier_data_input_1" {
+#   description = "memtier data input (1st)"
+#   default = "memtier_benchmark -x 3 -n 180000 -c 1 -t 1 --ratio=1:0 --data-size=80 --key-maximum=180000 --pipeline=1000 --key-pattern=S:S --hide-histogram"
+# }
+
+# variable "memtier_benchmark_1" {
+#   description = "memtier benchmark code to run (1st)"
+#   default = "memtier_benchmark -x 2 -t 8 -c 100 -n 100 --ratio=1:10000 --data-size=80 --key-maximum=180000 --hide-histogram"
+# }
+
+# variable "outfile_name_1" {
+#     description = "outfile json name (1st run)"
+#     default = "mybenchmarkOutfile.json"
+# }
+
+# # cluster db commands
+
+# variable "redis_db_name_1" {
+#     description = "redis enterprise db "
+#     default     = "myDB"
+# }
+
+# variable "redis_db_memory_size_1" {
+#     description = "redis enterprise db "
+#     default     = 100000000
+# }
+
+# variable "redis_db_replication_1" {
+#     description = "redis enterprise db "
+#     default     = "true"
+# }
+
+# variable "redis_db_sharding_1" {
+#     description = "redis enterprise db "
+#     default     = "false"
+# }
+
+# variable "redis_db_shard_count_1" {
+#     description = "redis enterprise db "
+#     default     = 1
+# }
+
+# variable "redis_db_proxy_policy_1" {
+#     description = "redis enterprise db "
+#     default     = "single"
+# }
+
+# variable "redis_db_shards_placement_1" {
+#     description = "redis enterprise db "
+#     default     = "dense"
+# }
+
+# variable "redis_db_data_persistence_1" {
+#     description = "redis enterprise db "
+#     default     = "aof"
+# }
+
+# variable "redis_db_aof_policy_1" {
+#     description = "redis enterprise db "
+#     default     = "appendfsync-always"
+# }
+
+# variable "redis_db_port" {
+#     description = "redis enterprise db "
+#     default     = 10000
+# }
