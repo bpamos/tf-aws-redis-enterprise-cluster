10c14b36946eefa683f38844f5cd8b785235b805
brandonamos <bpamos21@gmail.com>
2022-09-21 15:41:44 -0600

    clean and remove individual ips

--- modules/re-cluster/variables.tf
+++ modules/re-cluster/variables.tf
@@ -1,4 +1,4 @@
-# ### OTHER
+#### OTHER
 variable "region" {
     description = "AWS region"
 }
@@ -7,53 +7,9 @@ variable "ssh_key_path" {
     description = "name of ssh key to be added to instance"
 }
 
-
+####### Create Cluster Variables
+####### Node and DNS outputs used to Create Cluster
 variable "dns_fqdn" {
-    description = "."
-    default = ""
-}
-
-variable "node_1_private_ip" {
-    description = "."
-    default = ""
-}
-
-variable "node_2_private_ip" {
-    description = "."
-    default = ""
-}
-
-variable "node_3_private_ip" {
-    description = "."
-    default = ""
-}
-
-variable "node_1_external_ip" {
-    description = "."
-    default = ""
-}
-
-variable "node_2_external_ip" {
-    description = "."
-    default = ""
-}
-
-variable "node_3_external_ip" {
-    description = "."
-    default = ""
-}
-
-variable "node_1_public_dns" {
-    description = "."
-    default = ""
-}
-
-variable "node_2_public_dns" {
-    description = "."
-    default = ""
-}
-
-variable "node_3_public_dns" {
     description = "."
     default = ""
 }
@@ -70,7 +26,7 @@ variable "re-node-eip-ips" {
     default = []
 }
 
-variable "re-node-public-dns" {
+variable "re-data-node-eip-public-dns" {
     type = list
     description = "."
     default = []
@@ -82,6 +38,7 @@ variable "re-data-node-info" {
     default = []
 }
 
+############# cluster commands
 variable "re_cluster_username" {
     description = "redis enterprise cluster username"
     default     = "admin@admin.com"
@@ -91,3 +48,4 @@ variable "re_cluster_password" {
     description = "redis enterprise cluster password"
     default     = "admin"
 }
+
