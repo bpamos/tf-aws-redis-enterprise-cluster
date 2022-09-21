99cb552db1492048d66e55eca0daa50ead00489b
brandonamos <bpamos21@gmail.com>
2022-09-21 09:38:30 -0600

    clean modules

--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+#### Provider variables
 variable "region" {
     description = "AWS region"
 }
@@ -6,6 +7,7 @@ variable "aws_creds" {
     description = "Access key and Secret key for AWS [Access Keys, Secret Key]"
 }
 
+#### Important variables
 variable "ssh_key_name" {
     description = "name of ssh key to be added to instance"
 }
@@ -18,14 +20,9 @@ variable "owner" {
     description = "owner tag name"
 }
 
-# VPC
+#### VPC
 variable "vpc_cidr" {
     description = "vpc-cidr"
-}
-
-# DNS
-variable "dns_hosted_zone_id" {
-    description = "DNS hosted zone Id"
 }
 
 variable "base_name" {
@@ -44,8 +41,13 @@ variable "subnet_azs" {
     description = "subnet availability zone"
     default = ["us-east-1a","us-east-1b","us-east-1c"]
 }
-# Test Instance Variables
 
+#### DNS
+variable "dns_hosted_zone_id" {
+    description = "DNS hosted zone Id"
+}
+
+#### Test Instance Variables
 variable "test-node-count" {
   description = "number of data nodes"
   default     = 1
@@ -56,12 +58,7 @@ variable "test_instance_type" {
     default = "t3.micro"
 }
 
-
-# Redis Enterprise Cluster Variables
-# redis enterpise software instance ami
-# you need to search aws marketplace, select the region, and grab ami id.
-# https://aws.amazon.com/marketplace/server/configuration?productId=412acaa0-2074-4156-93a4-576366bbf396&ref_=psb_cfg_continue
-
+#### Redis Enterprise Cluster Variables
 variable "data-node-count" {
   description = "number of data nodes"
   default     = 3
@@ -82,26 +79,13 @@ variable "node-root-size" {
   default     = "50"
 }
 
-# EBS volume for persistent and ephemeral storage
-
-### must import "local variable to use this"
-# variable "enable-volumes" {
-#   description = "Enable EBS Devices for Ephemeral and Persistent storage"
-#   default     = true
-# }
-# variable "enable-volumes" {
-#   description = "Enable EBS Devices for Ephemeral and Persistent storage"
-#   default     = true
-# }
-
-
+#### EBS volume for persistent and ephemeral storage
 variable "re-volume-size" {
   description = "The size of the ephemeral and persistent volumes to attach"
   default     = "150"
 }
 
 #### Security
-
 variable "open-nets" {
   type        = list(any)
   description = "CIDRs that will have access to everything"
@@ -316,54 +300,9 @@ variable "external-rules" {
   ]
 }
 
-# ### OTHER
-
+####### Create Cluster Variables
+####### Node and DNS outputs used to Create Cluster
 variable "dns_fqdn" {
-    description = "."
-    default = ""
-}
-
-variable "node_1_private_ip" {
-    description = "."
-    default = ""
-}
-
-variable "node_2_private_ip" {
-    description = "."
-    default = ""
-}
-
-variable "node_3_private_ip" {
-    description = "."
-    default = ""
-}
-
-variable "node_1_external_ip" {
-    description = "."
-    default = ""
-}
-
-variable "node_2_external_ip" {
-    description = "."
-    default = ""
-}
-
-variable "node_3_external_ip" {
-    description = "."
-    default = ""
-}
-
-variable "node_1_public_dns" {
-    description = "."
-    default = ""
-}
-
-variable "node_2_public_dns" {
-    description = "."
-    default = ""
-}
-
-variable "node_3_public_dns" {
     description = "."
     default = ""
 }
@@ -386,26 +325,13 @@ variable "re-node-public-dns" {
     default = []
 }
 
-# variable "re-data-node-info" {
-#     type = map(object({
-#         dns = string
-#         eip = string
-#         internal-ip = string
-#     }))
-#     description = "."
-#     default = {}
-# }
 variable "re-data-node-info" {
     type = list
     description = "."
     default = []
 }
 
-# ##############################
-
-
-# # cluster commands
-
+############# cluster commands
 variable "re_cluster_username" {
     description = "redis enterprise cluster username"
     default     = "admin@admin.com"
@@ -415,55 +341,3 @@ variable "re_cluster_password" {
     description = "redis enterprise cluster password"
     default     = "admin"
 }
-
-# # cluster db commands
-
-# variable "redis_db_name_1" {
-#     description = "redis enterprise db "
-#     default     = "myDB"
-# }
-
-# variable "redis_db_memory_size_1" {
-#     description = "redis enterprise db "
-#     default     = 100000000
-# }
-
-# variable "redis_db_replication_1" {
-#     description = "redis enterprise db "
-#     default     = "true"
-# }
-
-# variable "redis_db_sharding_1" {
-#     description = "redis enterprise db "
-#     default     = "false"
-# }
-
-# variable "redis_db_shard_count_1" {
-#     description = "redis enterprise db "
-#     default     = 1
-# }
-
-# variable "redis_db_proxy_policy_1" {
-#     description = "redis enterprise db "
-#     default     = "single"
-# }
-
-# variable "redis_db_shards_placement_1" {
-#     description = "redis enterprise db "
-#     default     = "dense"
-# }
-
-# variable "redis_db_data_persistence_1" {
-#     description = "redis enterprise db "
-#     default     = "aof"
-# }
-
-# variable "redis_db_aof_policy_1" {
-#     description = "redis enterprise db "
-#     default     = "appendfsync-always"
-# }
-
-# variable "redis_db_port" {
-#     description = "redis enterprise db "
-#     default     = 10000
-# }
