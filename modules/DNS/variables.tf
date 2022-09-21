e0362e387a80ba4b704dfc4b3f74ec4900355ed1
brandonamos <bpamos21@gmail.com>
2022-09-21 09:11:09 -0600

    clean DNS

--- modules/DNS/variables.tf
+++ modules/DNS/variables.tf
@@ -5,19 +5,9 @@ variable "dns_hosted_zone_id" {
     description = "DNS hosted zone Id"
 }
 
-# variable "region" {
-#     description = "AWS region"
-#     default = "us-east-1"
-# }
-
 variable "vpc_name" {
   description = "The VPC Name tag"
 }
-
-# variable "base_name" {
-#     description = "base name for resources"
-#     default = "redisuser1-tf"
-# }
 
 variable "data-node-count" {
   description = "number of data nodes"
