a46d3228705d19ddcbe005bc46154aa56dcc2ee8
brandonamos <bpamos21@gmail.com>
2022-09-23 07:19:16 -0600

    remove unused variable

--- modules/nodes/outputs.tf
+++ modules/nodes/outputs.tf
@@ -10,7 +10,3 @@ output "re-data-node-internal-ips" {
 output "re-data-node-eip-public-dns" {
   value = aws_eip.re_cluster_instance_eip[*].public_dns #ec2 public dns after EIP association
 }
-
-output "re-data-node-info" {
-  value = formatlist("%s internal_ip=%s external_ip=%s",aws_eip.re_cluster_instance_eip[*].public_dns, aws_instance.re_cluster_instance[*].private_ip, aws_eip.re_cluster_instance_eip[*].public_ip)
-}
