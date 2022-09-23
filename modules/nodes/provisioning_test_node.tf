2012663fa5a740eac2735272eddba6a0f4444984
brandonamos <bpamos21@gmail.com>
2022-09-23 07:23:39 -0600

    convert template_file to local_file templatefile

--- modules/nodes/provisioning_test_node.tf
+++ modules/nodes/provisioning_test_node.tf
@@ -15,46 +15,30 @@ resource "null_resource" "remote-config-test" {
     }
   depends_on = [aws_instance.test_node, 
                 aws_eip_association.test_eip_assoc, 
-                null_resource.inventory_setup_test, 
-                null_resource.ssh-setup-test]
+                local_file.inventory_setup_test, 
+                local_file.ssh_setup_test]
 }
 
 #######################
-# Template Data
-data "template_file" "ansible_test_inventory" {
-  count    = var.test-node-count
-  template = "${file("${path.module}/inventory.tpl")}"
-  vars = {
-    host_ip  = element(aws_eip.test_node_eip.*.public_ip, count.index)
-    vpc_name = var.vpc_name
-    ncount   = count.index
-  }
-  depends_on = [aws_instance.test_node, aws_eip_association.test_eip_assoc]
+#########
+#### Generate Ansible Inventory for each node
+resource "local_file" "inventory_setup_test" {
+    count    = var.test-node-count
+    content  = templatefile("${path.module}/inventory.tpl", {
+        host_ip  = element(aws_eip.test_node_eip.*.public_ip, count.index)
+        vpc_name = var.vpc_name
+    })
+    filename = "/tmp/${var.vpc_name}_test_node_${count.index}.ini"
+    depends_on = [aws_instance.test_node, aws_eip_association.test_eip_assoc]
 }
 
-data "template_file" "ssh_config_test" {
-  template = "${file("${path.module}/ssh.tpl")}"
-  vars = {
-    vpc_name = var.vpc_name
-  }
-  depends_on = [aws_instance.test_node, aws_eip_association.test_eip_assoc]
-}
-
-###############################################################################
-# Template Write
-resource "null_resource" "inventory_setup_test" {
-  count = var.test-node-count
-  provisioner "local-exec" {
-    command = "echo \"${element(data.template_file.ansible_test_inventory.*.rendered, count.index)}\" > /tmp/${var.vpc_name}_test_node_${count.index}.ini"
-  }
-  depends_on = [data.template_file.ansible_test_inventory]
-}
-
-resource "null_resource" "ssh-setup-test" {
-  provisioner "local-exec" {
-    command = "echo \"${data.template_file.ssh_config_test.rendered}\" > /tmp/${var.vpc_name}_test_node.cfg"
-  }
-  depends_on = [data.template_file.ssh_config_test]
+#### Generate ansible.cfg file
+resource "local_file" "ssh_setup_test" {
+    content  = templatefile("${path.module}/ssh.tpl", {
+        vpc_name = var.vpc_name
+    })
+    filename = "/tmp/${var.vpc_name}_test_node.cfg"
+    depends_on = [aws_instance.test_node, aws_eip_association.test_eip_assoc]
 }
 
 ######################
