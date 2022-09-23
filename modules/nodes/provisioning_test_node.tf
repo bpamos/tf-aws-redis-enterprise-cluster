60fe4be5ab877c9d88f62b92d5ef1d7d850090c4
brandonamos <bpamos21@gmail.com>
2022-09-23 13:46:28 -0600

    local_file and sleeper

--- modules/nodes/provisioning_test_node.tf
+++ modules/nodes/provisioning_test_node.tf
@@ -1,6 +1,17 @@
 ############################
-# instead of running this inside aws_instance, run it outside with null resource
-# ensure we can get to the node first
+
+#### Sleeper, after instance, eip assoc, local file inventories & cfg created
+#### otherwise it can run to fast, not find the inventory file and fail or hang
+resource "time_sleep" "wait_30_seconds_test" {
+  create_duration = "30s"
+  depends_on = [aws_instance.test_node, 
+                aws_eip_association.test_eip_assoc, 
+                local_file.inventory_setup_test, 
+                local_file.ssh-setup-test]
+}
+
+
+# remote-config waits till the node is accessible
 resource "null_resource" "remote_config_test" {
   count = var.test-node-count
   provisioner "remote-exec" {
@@ -14,90 +25,41 @@ resource "null_resource" "remote_config_test" {
         }
     }
 
-    # depends_on = [aws_instance.test_node, 
-    #             aws_eip_association.test_eip_assoc, 
-    #             local_file.inventory_setup_test, 
-    #             local_file.ssh_setup_test]
-  depends_on = [aws_instance.test_node, 
+    depends_on = [aws_instance.test_node, 
                 aws_eip_association.test_eip_assoc, 
-                null_resource.inventory_setup_test, 
-                null_resource.ssh-setup-test]
+                local_file.inventory_setup_test, 
+                local_file.ssh-setup-test,
+                time_sleep.wait_30_seconds_test]
 }
 
-  # depends_on = [aws_instance.test_node, 
-  #               aws_eip_association.test_eip_assoc, 
-  #               local_file.inventory_setup_test, 
-  #               local_file.ssh_setup_test]
 
-#######################
-# #########
+
 # #### Generate Ansible Inventory for each node
-# resource "local_file" "inventory_setup_test" {
-#     count    = var.test-node-count
-#     content  = templatefile("${path.module}/inventory.tpl", {
-#         host_ip  = element(aws_eip.test_node_eip.*.public_ip, count.index)
-#         vpc_name = var.vpc_name
-#     })
-#     filename = "/tmp/${var.vpc_name}_test_node_${count.index}.ini"
-#     depends_on = [aws_instance.test_node, aws_eip_association.test_eip_assoc]
-# }
+resource "local_file" "inventory_setup_test" {
+    count    = var.test-node-count
+    content  = templatefile("${path.module}/inventory.tpl", {
+        host_ip  = element(aws_eip.test_node_eip.*.public_ip, count.index)
+        vpc_name = var.vpc_name
+    })
+    filename = "/tmp/${var.vpc_name}_test_node_${count.index}.ini"
+  depends_on = [aws_instance.test_node, aws_eip_association.test_eip_assoc]
+}
 
 # #### Generate ansible.cfg file
-# resource "local_file" "ssh_setup_test" {
-#     content  = templatefile("${path.module}/ssh.tpl", {
-#         vpc_name = var.vpc_name
-#     })
-#     filename = "/tmp/${var.vpc_name}_test_node.cfg"
-#     depends_on = [aws_instance.test_node, aws_eip_association.test_eip_assoc]
-# }
-
-
-# #######################
-# Template Data
-data "template_file" "ansible_test_inventory" {
-  count    = var.test-node-count
-  template = "${file("${path.module}/inventory.tpl")}"
-  vars = {
-    host_ip  = element(aws_eip.test_node_eip.*.public_ip, count.index)
-    vpc_name = var.vpc_name
-    ncount   = count.index
-  }
+resource "local_file" "ssh-setup-test" {
+    content  = templatefile("${path.module}/ssh.tpl", {
+        vpc_name = var.vpc_name
+    })
+    filename = "/tmp/${var.vpc_name}_test_node.cfg"
   depends_on = [aws_instance.test_node, aws_eip_association.test_eip_assoc]
 }
-
-data "template_file" "ssh_config_test" {
-  template = "${file("${path.module}/ssh.tpl")}"
-  vars = {
-    vpc_name = var.vpc_name
-  }
-  depends_on = [aws_instance.test_node, aws_eip_association.test_eip_assoc]
-}
-
-###############################################################################
-# Template Write
-resource "null_resource" "inventory_setup_test" {
-  count = var.test-node-count
-  provisioner "local-exec" {
-    command = "echo \"${element(data.template_file.ansible_test_inventory.*.rendered, count.index)}\" > /tmp/${var.vpc_name}_test_node_${count.index}.ini"
-  }
-  depends_on = [data.template_file.ansible_test_inventory]
-}
-
-resource "null_resource" "ssh-setup-test" {
-  provisioner "local-exec" {
-    command = "echo \"${data.template_file.ssh_config_test.rendered}\" > /tmp/${var.vpc_name}_test_node.cfg"
-  }
-  depends_on = [data.template_file.ssh_config_test]
-}
-
-
 
 ######################
-# Run some ansible
+# Run ansible playbook to install redis and memtier
 resource "null_resource" "ansible_test_run" {
   count = var.test-node-count
   provisioner "local-exec" {
     command = "ansible-playbook ${path.module}/ansible/testnode.yaml --private-key ${var.ssh_key_path} -i /tmp/${var.vpc_name}_test_node_${count.index}.ini"
   }
-  depends_on = [null_resource.remote_config_test]
+  depends_on = [null_resource.remote_config_test, time_sleep.wait_30_seconds_test]
 }
